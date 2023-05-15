5acfb1361e3436befe03976344e18b8baba99406
Ivan <us.ivan05@gmail.com>
2023-05-15 21:46:58 +0800

    its not work commit #4

--- LAUNCH_BOT.py
+++ LAUNCH_BOT.py
@@ -8,11 +8,15 @@ from create_bot import dp, bot
 
 """Главный модуль"""
 
-async def on_startup(dp):
-    await bot.set_webhook(config.webhook_uri, certificate=open("cert.pem", 'rb'))
 
+async def on_startup(dp):
+    await bot.set_webhook(config.webhook_url)
+
+
+# certificate=open("cert.pem", 'rb')
 async def on_shutdown(dp):
     await bot.delete_webhook()
+
 
 from handlers import commands
 from FSM_modules import registration
@@ -24,7 +28,6 @@ from FSM_modules import fsm_other
 from task_loop_functions import deadlines_notification, change_current_date
 
 if __name__ == "__main__":
-
     from data import institutes_and_groups
 
     from create_data_base import DB
@@ -34,8 +37,8 @@ if __name__ == "__main__":
     print("data base is init!")
 
     loop = get_event_loop()
-    loop.create_task(deadlines_notification(60*60*3))
-    loop.create_task(change_current_date(60*60*6))
+    loop.create_task(deadlines_notification(60 * 60 * 3))
+    loop.create_task(change_current_date(60 * 60 * 6))
 
     executor.start_webhook(
         dispatcher=dp,
@@ -43,6 +46,7 @@ if __name__ == "__main__":
         on_startup=on_startup,
         on_shutdown=on_shutdown,
         skip_updates=True,
-        port=443)
+        host=config.webapp_host,
+        port=config.webhook_port)
 
     print("Congrats, bot is actually working!!!")
