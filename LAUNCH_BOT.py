32c6210954e27c4ce648fc10a6d01f7c93b0a527
Ivan <us.ivan05@gmail.com>
2023-05-16 15:43:07 +0800

    last step maybe

--- LAUNCH_BOT.py
+++ LAUNCH_BOT.py
@@ -1,29 +1,44 @@
-import os
+import logging
 
 from aiogram.utils import executor
 from asyncio import get_event_loop
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
 
 import config
 from create_bot import dp, bot
 
 """Главный модуль"""
 
+logging.basicConfig(level=logging.INFO)
+dp.middleware.setup(LoggingMiddleware())
 
 async def on_startup(dp):
-    await bot.set_webhook(config.webhook_url, certificate=open("cert.pem", 'rb'))
+    await bot.set_webhook(config.webhook_url)
+    # insert code here to run it after start
 
 
-#
 async def on_shutdown(dp):
+    logging.warning('Shutting down..')
+
+    # insert code here to run it before shutdown
+
+    # Remove webhook (not acceptable in some cases)
     await bot.delete_webhook()
 
+    # Close DB connection (if used)
+    await dp.storage.close()
+    await dp.storage.wait_closed()
 
+    logging.warning('Bye!')
+
+#BOT BODY
 from handlers import commands
 from FSM_modules import registration
 from FSM_modules import calendar
 from handlers import text_commands
 from FSM_modules import make_admin
 from FSM_modules import fsm_other
+#/BOT BODY
 
 from task_loop_functions import deadlines_notification, change_current_date
 
